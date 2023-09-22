607c508b9c2b8285abb8b5b7991ae40f6567ff73
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-09-21 20:56:41 -0400

    added some java code

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Addition {
+    public static void main(String[] args){
+
+        int num1 = 11;
+        int num2 = 2;
+
+        //11/2  --> 5.5
+
+        //10+15 = 25
+        double div = num1 / num2;
+        // operator --> operand
+        //System.out.println(sum);
+        // multiplication -- division . subtraction
+
+        // concatenation --> to join
+        // overloaded operation --> +
+        System.out.println("Sum of num1 and num2 :" + div);
+
+    }
+}
+
